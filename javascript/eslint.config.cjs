52436f1c29a05155eda4e4bae0150b4bf0985f18
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-06-28 17:25:11 +0500

    Add Jest plugin

--- javascript/eslint.config.cjs
+++ javascript/eslint.config.cjs
@@ -1,9 +1,11 @@
 const globals = require('globals');
 const pluginJs = require('@eslint/js');
 const eslintConfigPrettier = require('eslint-config-prettier');
+const eslintPluginJest = require('eslint-plugin-jest');
 
 module.exports = [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  eslintPluginJest,
   eslintConfigPrettier,
 ];

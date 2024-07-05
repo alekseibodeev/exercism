3c901c98b6967c5ef8a5f443a125f1e1ac8efaba
Aleksei Bodeev <bodeevaleksei@gmail.com>
2024-07-05 07:54:51 +0500

    Fix eslint config

--- javascript/eslint.config.cjs
+++ javascript/eslint.config.cjs
@@ -1,11 +1,14 @@
 const globals = require('globals');
 const pluginJs = require('@eslint/js');
 const eslintConfigPrettier = require('eslint-config-prettier');
-const eslintPluginJest = require('eslint-plugin-jest');
+const pluginJest = require('eslint-plugin-jest');
 
 module.exports = [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  eslintPluginJest,
+  pluginJest.configs['flat/recommended'],
   eslintConfigPrettier,
+  {
+    ignores: ['**/*.config.*', '/cleanup.js', '**/*.spec.js'],
+  },
 ];
